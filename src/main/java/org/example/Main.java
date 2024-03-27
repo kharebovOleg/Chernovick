410a002ee96371877a7095d3cbc4f77e0ea60610
kharebovOleg <xarebov1993@yandex.ru>
2024-03-27 20:57:22 +0300

    added Tries

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
@@ -12,6 +14,7 @@ public class Main {
     static public final int number = 100;
 
     public static void main(String[] args) throws IOException {
+
 
     }
 }
---
+++ src/main/java/org/example/hexlet/Algoritms/Trees/Trie/Main.java
@@ -0,0 +1,54 @@
+package org.example.hexlet.Algoritms.Trees.Trie;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get("src/main/resources/tries/numbers.txt"));
+
+        TrieNode root = new TrieNode(' ');
+        for (String line : lines) {
+            root.insert(line);
+        }
+
+        // System.out.println(root.containsString("18АР2970"));
+
+        writeTrieToFile("src/main/resources/tries/out.txt", root);
+
+        TrieNode fromFile = readFromFile("src/main/resources/tries/out.txt");
+
+        List<String> extractedFromTrie = new ArrayList<>();
+        fromFile.getAllNumbers("", extractedFromTrie);
+    }
+
+    public static void writeTrieToFile(String path, TrieNode root) {
+        try {
+            PrintWriter out = new PrintWriter(path);
+            root.writeToFile(out);
+            out.flush();
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static TrieNode readFromFile(String path) {
+        TrieNode root = new TrieNode(' '); // создаем корневой узел
+        try {
+            FileReader reader = new FileReader(path); // создаем fileReader
+            reader.read(); // пропускаем первый символ из файла
+            root.readFromFile(reader);
+            reader.close();
+        } catch (IOException e) {
+           e.printStackTrace();
+        }
+        return root;
+    }
+}
