173e0e99f094fdd69cb41970a45c1702147bc88c
kharebovOleg <xarebov1993@yandex.ru>
2024-03-29 18:25:20 +0300

    KDTrees

--- src/main/java/org/example/hexlet/Algoritms/Trees/KDTree/Helper.java
+++ src/main/java/org/example/hexlet/Algoritms/Trees/KDTree/Helper.java
@@ -1,2 +1,68 @@
-package org.example.hexlet.Algoritms.Trees.KDTree;public class Helper {
+package org.example.hexlet.Algoritms.Trees.KDTree;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
+public class Helper {
+
+    /*
+    Данный код представляет собой реализацию метода buildTree класса KDTreeNode, который строит KD-дерево
+    на основе переданных точек.
+
+    Аргументы метода:
+
+    points - список точек, представленных в виде словарей (Map<String, Integer>), где ключ - это имя измерения,
+    а значение - значение точки по данному измерению.
+    depth - текущая глубина построения дерева.
+    dimensions - список имен измерений.
+    parent - родительский узел.
+     */
+    public static KDTreeNode buildTree(
+            List<Map<String, Integer>> points, int depth, List<String> dimensions, KDTreeNode parent) {
+
+        // 1) В начале метода определяется текущая ось разбиения пространства, используя остаток от деления глубины
+        // на количество измерений. Например, если глубина равна 0, а количество измерений равно 3, то текущая ось
+        // будет иметь индекс 0.
+        var dim = dimensions.get(depth % dimensions.size());
+
+        // 2) Затем проверяется базовый случай, когда список точек пуст. В этом случае возвращается null, так как нет
+        // точек для построения узла дерева.
+        if (points.size() == 0) {
+            return null;
+        }
+
+        // 3) Далее проверяется другой базовый случай, когда в списке точек есть только одна точка. В этом случае
+        // создается новый узел дерева с этой точкой, текущей осью разбиения и родительским узлом.
+        if (points.size() == 1) {
+            return new KDTreeNode(points.get(0), dim, parent);
+        }
+
+        // 4) Затем точки в списке сортируются по значению текущей оси разбиения. Для этого используется
+        // лямбда-выражение, которое сравнивает значения точек по текущей оси.
+        points.sort(Comparator.comparingInt(a -> a.get(dim)));
+
+        // 5) Вычисляется медиана списка точек и создается новый узел дерева с этой медианной точкой,
+        // текущей осью разбиения и родительским узлом
+        int median = points.size() / 2;
+        KDTreeNode node = new KDTreeNode(points.get(median), dim, parent);
+
+        // 6) Затем рекурсивно вызывается метод buildTree для левого и правого подпространств, передавая
+        // соответствующие подсписки точек, увеличенную глубину и текущую ось разбиения. Родительским узлом
+        // для левого и правого поддеревьев становится только что созданный узел.
+        node.left = buildTree(points.subList(0, median), depth + 1, dimensions, node);
+        node.right = buildTree(points.subList(median + 1, points.size()), depth + 1, dimensions, node);
+
+        // 7) В конце метода возвращается созданный узел.
+        return node;
+    }
+
+    public static List<Map<String, Integer>> run(
+            List<Map<String, Integer>> points, Map<String, Integer> center, int radius) {
+
+        List<String> dimensions = List.of("x", "y");
+        KDTreeNode tree = buildTree(points, 0, dimensions, null);
+        return Main.findPointsInRadius(tree, center, radius);
+    }
 }
--- src/main/java/org/example/hexlet/Algoritms/Trees/KDTree/Main.java
+++ src/main/java/org/example/hexlet/Algoritms/Trees/KDTree/Main.java
@@ -1,2 +1,64 @@
-package org.example.hexlet.Algoritms.Trees.KDTree;public class Main {
+package org.example.hexlet.Algoritms.Trees.KDTree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/*
+Метод принимает три параметра:
+
+KD дерево с точками, объект класса KDTreeNode
+Точку – центр окружности. Точка – это словарь Map<String, Integer> со свойствами x и y
+Радиус окружности, целое число
+Функция должна вернуть список List<Map<String, Integer>> всех точек из дерева, которые находятся внутри окружности
+ */
+public class Main { // todo разобраться с работой метода
+    public static List<Map<String, Integer>> findPointsInRadius(KDTreeNode node, Map<String, Integer> centerPoint, int radius) {
+        if (node == null) {
+            return new ArrayList<>();
+        }
+        // в данном методе координаты x и y являются измерениями - dimension
+        // определяем лежит ли точка в пределах окружности по формуле
+        // (x - center_x)^2 + (y - center_y)^2 <= radius^2
+
+        double dx = centerPoint.get("x") - node.point.get("x"); // (x - center_x)
+        double dy = centerPoint.get("y") - node.point.get("y"); // (y - center_y)
+
+        double distanceSquared = dx * dx + dy * dy; // (x - center_x)^2 + (y - center_y)^2
+
+        if (distanceSquared <= radius * radius) { // если точка принадлежит окружности
+            List<Map<String, Integer>> result = new ArrayList<>(); // создаем итоговый список
+            result.add(node.point); // добавляем узел в итоговый список
+            result.addAll(findPointsInRadius(node.left, centerPoint, radius)); // повторяем все действия для дочерних узлов
+            result.addAll(findPointsInRadius(node.right, centerPoint, radius));
+            return result;
+        }
+        // Теперь, если точка находится вне радиуса.
+        // В зависимости от значения dimension, мы делаем выбор, в каком поддереве искать дальше - либо в левом,
+        // либо в правом. Таким образом, мы оставляем за пределами нашего интереса только часть дерева, в которой могут
+        // находиться точки внутри заданного радиуса.
+
+        if (node.dimension.equals("x") && centerPoint.get("x") - radius <= node.point.get("x")) {
+            List<Map<String, Integer>> result = new ArrayList<>();
+            result.addAll(findPointsInRadius(node.left, centerPoint, radius));
+            result.addAll(findPointsInRadius(node.right, centerPoint, radius));
+            return result;
+
+        }
+
+        if (node.dimension.equals("y") && centerPoint.get("y") - radius <= node.point.get("y")) {
+            List<Map<String, Integer>> result = new ArrayList<>();
+            result.addAll(findPointsInRadius(node.left, centerPoint, radius));
+            result.addAll(findPointsInRadius(node.right, centerPoint, radius));
+            return result;
+
+        }
+
+        return findPointsInRadius(node.right, centerPoint, radius);
+    }
+
+    public static List<Map<String, Integer>> run(
+            List<Map<String, Integer>> points, Map<String, Integer> center, int radius) {
+        return Helper.run(points, center, radius);
+    }
 }
